d07533c2c321a7bd6da2e210c5553561bef0329f
Sydney Stokes <sydneystokes@Sydneys-MacBook-Pro-5.local>
2022-04-30 22:19:10 -0400

    Deleted extra lines of code and added submit button

--- event.py
+++ event.py
@@ -5,9 +5,7 @@ from kivy.uix.textinput import TextInput
 
 class MainApp(App):
     def build(self):
-        self.operators = ["/", "*", "+", "-"]
-        self.last_was_operator = None
-        self.last_button = None
+        
         main_layout = BoxLayout(orientation="vertical")
         self.eventName = TextInput(
             multiline=False, readonly=False, halign="center", font_size=55, text="Enter the event name"
@@ -33,54 +31,15 @@ class MainApp(App):
         main_layout.add_widget(self.eventEndTime)
         main_layout.add_widget(self.eventDescription)
         main_layout.add_widget(self.eventContact)
-        buttons = [
-            ["7", "8", "9", "/"],
-            ["4", "5", "6", "*"],
-            ["1", "2", "3", "-"],
-            [".", "0", "C", "+"],
-        ]
-        for row in buttons:
-            h_layout = BoxLayout()
-            for label in row:
-                button = Button(
-                    text=label,
-                    pos_hint={"center_x": 0.5, "center_y": 0.5},
-                )
-                button.bind(on_press=self.on_button_press)
-                h_layout.add_widget(button)
-            main_layout.add_widget(h_layout)
+        
 
-        equals_button = Button(
-            text="=", pos_hint={"center_x": 0.5, "center_y": 0.5}
+        submit_button = Button(
+            text="Submit", pos_hint={"center_x": 0.5, "center_y": 0.5}
         )
-        equals_button.bind(on_press=self.on_submit)
-        main_layout.add_widget(equals_button)
+        submit_button.bind(on_press=self.on_submit)
+        main_layout.add_widget(submit_button)
 
         return main_layout
-
-    def on_button_press(self, instance):
-        current = self.eventName.text
-        button_text = instance.text
-
-        if button_text == "C":
-            # Clear the solution widget
-            self.eventName.text = ""
-        else:
-            if current and (
-                self.last_was_operator and button_text in self.operators):
-                # Don't add two operators right after each other
-                return
-            elif current == "" and button_text in self.operators:
-                # First character cannot be an operator
-                return
-            else:
-                new_text = current + button_text
-                self.eventName.text = new_text
-        self.last_button = button_text
-        self.last_was_operator = self.last_button in self.operators
-
-    #def displayEvent(self, name, date, start, end, description, contact):
-    #   pass;
 
     #on_submit assign all variable values
     def on_submit(self, instance):
@@ -90,8 +49,16 @@ class MainApp(App):
         inputET = self.eventEndTime.text
         inputEDes = self.eventDescription.text
         inputEC = self.eventContact.text
-
-       #displayEvent(inputEN, inputED, inputST, inputET, inputEDes, inputEC)
+        
+        self.display_event(inputEN, inputED, inputST, inputET, inputEDes, inputEC)
+    
+    def display_event(self, name, date, start, end, description, contact):
+        print(name)
+        print(date)
+        print(start)
+        print(end)
+        print(description)
+        print(contact)
 
     
 
